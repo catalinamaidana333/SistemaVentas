f27be2b3c3ae3d1d35355a1654c13fbf9d35e4e5
catalinamaidana333 <catalinamaidana18@gmail.com>
2026-08-30 15:55:47 -0300

    Add Rol and Usuario entity classes

---
+++ SistemaVentas.Entities/Rol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaVentas.Entities
+{
+    public class Rol
+    {
+        public int IdRol { get; set; }
+        public string Nombre { get; set; }
+    }
+}
