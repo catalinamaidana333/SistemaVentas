d43601a3a24baa821f07a9d6d851722a6d42e0d5
catalinamaidana333 <catalinamaidana18@gmail.com>
2026-09-22 23:23:54 -0300

    elimino de cabecera referencia a capa DAL

--- SistemaVentas.GUI/Login.xaml.cs
+++ SistemaVentas.GUI/Login.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 using SistemaVentas.BLL;
-using SistemaVentas.DAL;
+
 using SistemaVentas.Entities;
 using SistemaVentas.GUI.Contexto;
 
